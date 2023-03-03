aea5f18147786a2784284b0260887321c3150141
zfaridr <zainullovfr@gmail.com>
2023-03-03 13:17:59 +0300

    user+todo

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/user.js'
 import ToDoList from './components/todo.js'
 import axios from 'axios'
 
@@ -8,7 +9,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'todo': []
+      'todo': [],
+      'user': []
     }
   }
 
@@ -38,16 +40,51 @@ class App extends React.Component {
         const todo = response.data
           this.setState(
           {
-            'todo': todo
+            'todo': todo,
           }
         )
       }).catch(error => console.log(error))
   }
   
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/todo/')
+      .then(response => {
+        const todo = response.data
+          this.setState(
+          {
+            'todo': todo,
+          }
+        )
+      }).catch(error => console.log(error))
+    axios.get('http://127.0.0.1:8000/api/user/')
+      .then(response => {
+        const user = response.data
+          this.setState(
+          {
+            'user': user,
+          }
+        )
+      }).catch(error => console.log(error))
+  }
+
+
   render() {
     return (
-      <div>
-        <ToDoList ToDo={this.state.todo} />
+      <div class='Wrapper'>
+        <div class='MainMenu'>
+          <div>
+            <ToDoList ToDo={this.state.todo} />
+          </div>
+          <div>
+            <UserList User={this.state.user} />
+          </div>
+        </div>
+      
+        <div class='MainFooter'>
+          <p>
+            Info on main page of the todo_rest
+          </p>
+        </div>
       </div>
     )
   }
